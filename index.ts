408ee78c9a674c1f6bda8092dad63ed5a0d63c00
MaybeMike1 <michaelberko3@gmail.com>
2026-05-23 15:09:59 +0200

    added package.json commands

--- index.ts
+++ index.ts
@@ -1,1 +1,7 @@
-console.log("Hello via Bun!");
+const server = Bun.serve({
+    routes: {
+        "/": () => new Response("Kodevagt 0.0.1"),
+    }
+});
+
+console.log(`Server running at ${server.url}`);
